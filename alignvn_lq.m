964c4d824b0ed8fa2f304a1bea82da54d8ca407c
braveryyyy <140806993+braveryyyy@users.noreply.github.com>
2023-07-29 13:42:53 +0800

    Update alignvn_lq.m

--- alignvn_lq.m
+++ alignvn_lq.m
@@ -22,11 +22,9 @@ function [att0, attk, xkpk,ins] = alignvn_lq(imu, qnb, pos, phi0, imuerr, wvn,in
 %	wvn = [0.01; 0.01; 0.01];
 %	[att0, attk, xkpk] = alignvn(imu, avp0(1:3)', avp0(7:9)', phi, imuerr, wvn);
 %
-% See also  alignfn, aligngps, alignfn9, aligncmps, aligni0, alignWahba, alignsb, alignvndp, etm.
 
-% Copyright(c) 2009-2014, by Gongmin Yan, All rights reserved.
-% Northwestern Polytechnical University, Xi An, P.R.China
-% 17/06/2011
+
+
 global glv
     if nargin<4,  phi0 = [1.5; 1.5; 3]*glv.deg;  end
     if nargin<5,  imuerr = imuerrset(0.01, 100, 0.001, 1);  end
